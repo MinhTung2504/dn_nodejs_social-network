2857f73b00d3b926fb82801cfe9da91b97f110a3
MinhTung2504 <minhtungg2504@gmail.com>
2021-10-15 10:32:52 +0700

    Connect DB

---
+++ ormconfig.ts
@@ -0,0 +1,18 @@
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+
+const config: MysqlConnectionOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT) || 3306,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  migrations: ['src/database/migrations/*.ts'],
+  cli: {
+    migrationsDir: 'src/database/migrations',
+  },
+};
+
+export default config;
